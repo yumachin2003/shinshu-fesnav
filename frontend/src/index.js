6bffcdeb9c50f4ed1f0f59ad98a368203b851862
j22006ra-alt <j22006ra@stu.nagano.ac.jp>
2025-12-15 02:17:31 +0900

    ダークモード・ライトモードの起動時表示をデバイス由来に

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { MantineProvider } from '@mantine/core';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,7 +10,9 @@ import '@mantine/core/styles.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider defaultColorScheme="auto">
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
 
